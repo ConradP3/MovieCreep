fc9f82fd639ac4a4bb548a062e1be2a1966156e7
colperei <colperei@ucsc.edu>
2021-06-02 09:32:45 -0700

    Private

---
+++ private/secret_settings.py
@@ -0,0 +1,14 @@
+# single sign on Google (will be used if provided)
+OAUTH2GOOGLE_CLIENT_ID = "1025099160763-55ahu63qunmfhknnen1nm8p4tereute3.apps.googleusercontent.com"
+OAUTH2GOOGLE_CLIENT_SECRET = "SeTDkjL-7PLDZwHTe1fPibGi"
+
+# moviecreep-1 
+# mtIa1Aa5P5tCH04J
+
+DB_CONNECTION = "moviecreep:us-west2:moviecreep-1"
+
+DB_USER = "appuser"
+
+DB_NAME = "MovieCreepdb"
+
+DB_PASSWORD = "mtIa1Aa5P5tCH04J"
