3b722d9d7b1f85fa63ac7df6e63d1ac1238949d5
colperei <colperei@ucsc.edu>
2021-06-02 17:25:20 -0700

    Quick add movies from recommendations to user's watch list

--- private/secret_settings.py
+++ private/secret_settings.py
@@ -1,5 +1,6 @@
 # single sign on Google (will be used if provided)
 OAUTH2GOOGLE_CLIENT_ID = "1025099160763-55ahu63qunmfhknnen1nm8p4tereute3.apps.googleusercontent.com"
+
 OAUTH2GOOGLE_CLIENT_SECRET = "SeTDkjL-7PLDZwHTe1fPibGi"
 
 # moviecreep-1 
