04f45799c7c000a2a2aa0789fdf4be127bf2dd69
anarang1 <ahana.narang@gmail.com>
2021-06-07 02:48:30 -0700

    Update models.py

--- models.py
+++ models.py
@@ -41,6 +41,7 @@ db.define_table(
     Field('watch_list_user_name', default=get_user_name),
     Field('watch_list_time_stamp', 'datetime', default=get_time),
     Field('watch_list_review'),
+    Field('watch_list_count'),
 )
 
 db.define_table('likes',
